6fd28ed41aba9644283b5fbe7c0269fc9d2b3d9a
dawidnowak11 <dawid_nowak_11@wp.pl>
2025-05-29 19:01:13 +0200

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // <- DODAJ TO TUTAJ
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: true
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
